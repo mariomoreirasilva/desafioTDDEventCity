c8ba768cdb673f64b2778400657672a67fda5c3e
mariomoreirasilva <mario@desban.org.br>
2024-07-04 18:05:22 -0300

    Insert City ok

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.demo.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
@@ -21,6 +25,16 @@ public class CityController {
 	public ResponseEntity<List<CityDTO>> findAll(){
 		List<CityDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto){
+		 dto = service.insert(dto);
+		 //colocar o cabeario da resposta de inserção
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		 //retorno com http 201(criado)
+		 return ResponseEntity.created(uri).body(dto);
+		 
 	}
 	
 }
--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -2,11 +2,11 @@ package com.devsuperior.demo.services;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.entities.City;
@@ -17,10 +17,20 @@ public class CityService {
 	@Autowired
 	private CityRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CityDTO> findAll(){
 	   	List<City> list = repository.findAll(Sort.by("name"));
 	   	
 	   	return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
+	
 
 }
